aee5addad875db4d4e4e3f0bf47f84d49a9a5840
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-08-11 14:50:08 +0100

    FIX: Refactor

---
+++ scripts/ui/actions_menu/action_options.gd
@@ -0,0 +1,41 @@
+extends HBoxContainer
+
+#TODO make ui invisible if character is selected
+
+func _ready():
+	var buttons := _get_buttons()
+
+	for button in buttons:
+		button.connect("pressed", _on_button_pressed.bind(button))
+
+	for submenu in get_parent().get_children():
+		if submenu != self:
+			submenu.visible = false
+
+
+func _on_button_pressed(button: Button):
+	_open_submenu(button.text)
+
+
+func _open_submenu(menu: String):
+	for submenu in get_parent().get_children():
+		if submenu == self:
+			continue
+
+		if menu == submenu.name:
+			if submenu.is_visible():
+				submenu.set_visible(false)
+			else:
+				submenu.set_visible(true)
+		else:
+			submenu.set_visible(false)
+
+
+func _get_buttons() -> Array:
+	var buttons := []
+
+	for child in get_children():
+		if child is Button:
+			buttons.append(child)
+
+	return buttons
